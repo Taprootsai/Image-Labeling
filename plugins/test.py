6f94a2f7d6e0145100e2f4efa70c88366543e82d
adityavsm-is19 <adityavsm.is19@bmsce.ac.in>
2022-08-31 18:42:28 +0530

    updated folder structure

--- plugins/test.py
+++ plugins/test.py
@@ -3,7 +3,7 @@ import cv2
 from Helpers.person_extraction import PersonExtraction
 
 pe = PersonExtraction("../downloaded_models/yolo.h5")
-racket,person,racket_mid,person_mid = pe.extract("1.png",0.75)
+racket,person,racket_mid,person_mid = pe.extract("../ref_images/tennis/backhand/1.png",0.75)
 cv2.imshow("person",person)
 cv2.waitKey()
 cv2.imshow("racket",racket)
