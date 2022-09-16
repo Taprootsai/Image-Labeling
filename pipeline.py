cf0087d6e100544a00c416876b8d5ffd85354f95
adityavsm-is19 <adityavsm.is19@bmsce.ac.in>
2022-09-16 15:10:08 +0530

    added support for folder of input images

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,4 @@
-from glob import glob
+import glob
 import sys
 import cv2
 import matplotlib.pyplot as plt
@@ -26,22 +26,16 @@ def _plot_graph(image):
     plt.show()
 
 if src_data_mode=="local":
-    for i in sorted(glob(src_data_path)):
+    alg = Algorithms()
+    class_name = alg.selection_algorithms_available[selection_algorithm.lower()]['class_name']
+    module_name = "selection_algorithms."+alg.selection_algorithms_available[selection_algorithm.lower()]["file_name"]            
+    selection_class = getattr(importlib.import_module(module_name), class_name)
+    selection_obj = selection_class()
+    
+    for i in sorted(glob.glob(src_data_path+'/*')):
         # _plot_graph(cv2.imread(i))
-        
-        alg = Algorithms()
-        class_name = alg.selection_algorithms_available[selection_algorithm.lower()]['class_name']
-        module_name = "selection_algorithms."+alg.selection_algorithms_available[selection_algorithm.lower()]["file_name"]            
-        selection_class = getattr(importlib.import_module(module_name), class_name)
-        
+           
         #TODO: Add support for scoring and bucketizing folder of test data(multiple test images maybe video)
-        
-        res_obj = selection_class()
-        individual_class_avg_scores = dict()
-        for ref_class_folder in sorted(glob(ref_data_path)):
-            individual_class_avg_scores[ref_class_folder.split("\\")[-1]] = 0
-            for ref_img in sorted(glob(ref_class_folder+'/*')):
-                individual_class_avg_scores[ref_class_folder.split("\\")[-1]] += res_obj.get_res(plugins, i, ref_img)
-                print(res_obj.get_res(plugins, i, ref_img))
+        print(selection_obj.get_res(plugins, i, ref_data_path))
         
         # print(selection_class)
--- selection_algorithms/highestMatch.py
+++ selection_algorithms/highestMatch.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('E:/Open source/Image-Labeling')
-
+import glob
 from selection_algorithms.algorithms import Algorithms
 
 class HighestMatch(Algorithms):
@@ -8,9 +8,26 @@ class HighestMatch(Algorithms):
         super().__init__()
     
     def get_res(self, plugins, img_path, ref_img_path):
-        plugin_scores = super().generate_scores(plugins, img_path, ref_img_path)
-        highest_match = 0
-        highest_match_plugin = ""
-        return plugin_scores
- 
-    
+        
+        available_classes = list()
+        for i in sorted(glob.glob(ref_img_path+'/*')):
+            available_classes.append(i.split("/")[-1])
+        
+        all_plugin_scores = super().generate_scores(plugins, img_path, ref_img_path)
+        print(all_plugin_scores)
+        avg = dict()#avg of all plugins for individual classes
+        
+        for classes in available_classes:
+            sum_ = 0    
+            for individual_plugin_score in all_plugin_scores.keys():
+                # print(all_plugin_scores[individual_plugin_score])
+                sum_ += all_plugin_scores[individual_plugin_score][classes]
+            avg[classes] = sum_/len(plugins)
+        print(avg)
+        highest_score = 0
+        highest_class = ""
+        for i in avg.keys():
+            if avg[i] > highest_score:
+                highest_score = avg[i]
+                highest_class = i
+        return highest_class, highest_score
